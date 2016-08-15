8235ce29d1d1bf979167de85991b78a9415e93a5
piciu <krzochalski@gmail.com>
2016-08-15 11:43:09 +0200

    added counting time to logout

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var loginForm = document.getElementById('login-form');
 var alertLoggedIn = document.getElementById('alert-loggedin');
 var alertLoggedOut = document.getElementById('alert-loggedout');
+var alertTime = document.getElementById('alert-time');
+var alertTimeCount = document.getElementById('alert-time-count');
 
 function checkIfLogged() {
     if (localStorage.getItem('loginTime') !== null) {
@@ -8,18 +10,24 @@ function checkIfLogged() {
         loginForm.style.display = 'none';
         alertLoggedIn.style.display = 'block';
         alertLoggedOut.style.display = 'none';
+        alertTime.style.display = 'block';
     } else {
         console.log('not logged');
         loginForm.style.display = 'block';
         alertLoggedIn.style.display = 'none';
         alertLoggedOut.style.display = 'block';
+        alertTime.style.display = 'none';
     }
 }
 
-function logout(event) {
+function clickLogout(event) {
     event.preventDefault();
+    logout();
+}
 
-    localStorage.clear();
+function logout() {
+    localStorage.removeItem('loginName');
+    localStorage.removeItem('loginTime');
     checkIfLogged();
 }
 
@@ -27,17 +35,47 @@ function logIn(event) {
     event.preventDefault();
 
     var login = document.getElementById('login-email').value,
-        loginTime = Date.now();
+        loginTime = Date.now(),
+        loginTimeUntilLogin = document.getElementById('login-time').value;
 
     localStorage.setItem('loginName', login);
     localStorage.setItem('loginTime', loginTime);
+    localStorage.setItem('loginTimeUntilLogin', loginTimeUntilLogin);
 
     checkIfLogged();
+    displayTimeToLogout(Number(localStorage.getItem('loginTimeUntilLogin')));
+}
+
+function countTimeToLogout(loginTime, timeUntilLogout) {
+    var reloadTime = Date.now(),
+        timeDifference = reloadTime - loginTime,
+        timeUntilLastLogin = timeDifference / 60000,
+        timerUntilLogout = timeUntilLogout - timeUntilLastLogin;
+
+    alertTimeCount.innerHTML = timerUntilLogout;
+    return timerUntilLogout;
+}
+
+function displayTimeToLogout() {
+    var time = Number(localStorage.getItem('loginTimeUntilLogin'));
+    var interval = 0;
+    countTimeToLogout(localStorage.getItem('loginTime'), time);
+    interval = setInterval(function () {
+        countTimeToLogout(localStorage.getItem('loginTime'), time);
+        console.log('interval');
+        if (countTimeToLogout(localStorage.getItem('loginTime'), time) <= 0) {
+            clearInterval(interval);
+            logoutAfterXMinutes(localStorage.getItem('loginTime'), time);
+        }
+    }, 1000);
+
+    logoutAfterXMinutes(localStorage.getItem('loginTime'), time);
 }
 
 window.onload = function () {
-    var time = 5;
-
     checkIfLogged();
-    logoutAfterXMinutes(localStorage.getItem('loginTime'), time);
+    displayTimeToLogout();
+
+    // logoutAfterXMinutes can also be called in window.onload for performance reasons
+    // logoutAfterXMinutes(localStorage.getItem('loginTime'), time);
 };
--- logout-after-x-minutes.js
+++ logout-after-x-minutes.js
@@ -10,6 +10,10 @@ function logoutAfterXMinutes(loginTime, timeUntilLogout) {
     if (timeUntilLastLogin >= timeUntilLogout) {
         localStorage.clear();
         alert(timeUntilLogout + ' minutes since last login has passed. User was logged out automatically.');
+        if (logout() !== undefined) {
+            logout();
+            checkIfLogged();
+        }
     } else {
         console.log('User is about to be logged out in ' + (timeUntilLogout - timeUntilLastLogin) + ' minutes.');
     }
